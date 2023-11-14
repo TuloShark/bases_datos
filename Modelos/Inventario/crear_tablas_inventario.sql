c571444d2fb8e494412a29e28589c51127b42e61
Zadok <sebasgranadosa@gmail.com>
2023-11-13 18:16:28 -0600

    Replicación de datos

--- Modelos/Inventario/crear_tablas_inventario.sql
+++ Modelos/Inventario/crear_tablas_inventario.sql
@@ -1,36 +1,73 @@
-
 -- Crear la tabla de Bodegas
 CREATE TABLE Bodegas (
-    bodegaId INT PRIMARY KEY,
+    bodegaId INT IDENTITY PRIMARY KEY,
     nombre NVARCHAR(100),
-    ubicacion NVARCHAR(255),
-    capacidad INT
+    ubicacion NVARCHAR(100)
+);
+
+-- Crear la tabla de Afinidades
+CREATE TABLE Afinidades (
+    afinidadId INT IDENTITY PRIMARY KEY,
+    descripcion NVARCHAR(100)
+);
+
+-- Crear la tabla de Empleados
+CREATE TABLE Empleados (
+    empleadoId INT IDENTITY PRIMARY KEY,
+    nombre NVARCHAR(100),
+    rol NVARCHAR(50),
+    salario DECIMAL(10, 2),
+    moneda NVARCHAR(3),
+    bodegaId INT,
+    FOREIGN KEY (bodegaId) REFERENCES Bodegas(bodegaId)
 );
 
 -- Crear la tabla de Productos
 CREATE TABLE Productos (
-    productoId INT PRIMARY KEY,
+    productoId INT IDENTITY PRIMARY KEY,
     nombre NVARCHAR(100),
     descripcion NVARCHAR(255),
-    precio DECIMAL(10, 2)
+    cantidad INT,
+    afinidadId INT,
+    FOREIGN KEY (afinidadId) REFERENCES Afinidades(afinidadId)
 );
 
--- Crear la tabla de Inventario
-CREATE TABLE Inventario (
-    inventarioId INT PRIMARY KEY,
-    productoId INT,
-    bodegaId INT,
-    cantidad INT,
-    FOREIGN KEY (productoId) REFERENCES Productos(productoId),
-    FOREIGN KEY (bodegaId) REFERENCES Bodegas(bodegaId)
-);
+-- Inserción de Afinidades en la tabla
+INSERT INTO Afinidades (descripcion) VALUES ('electronica');
+INSERT INTO Afinidades (descripcion) VALUES ('ropa');
+INSERT INTO Afinidades (descripcion) VALUES ('alimentos');
+INSERT INTO Afinidades (descripcion) VALUES ('hogar');
+INSERT INTO Afinidades (descripcion) VALUES ('ferreteria');
+INSERT INTO Afinidades (descripcion) VALUES ('juguetes');
+INSERT INTO Afinidades (descripcion) VALUES ('libros');
+INSERT INTO Afinidades (descripcion) VALUES ('deportes');
+INSERT INTO Afinidades (descripcion) VALUES ('automotriz');
 
--- Crear la tabla de Movimientos de Inventario
-CREATE TABLE MovimientosInventario (
-    movimientoId INT PRIMARY KEY,
-    inventarioId INT,
-    tipoMovimiento NVARCHAR(50),
-    cantidad INT,
-    fechaMovimiento DATETIME,
-    FOREIGN KEY (inventarioId) REFERENCES Inventario(inventarioId)
-);
+-- Bodegas
+INSERT INTO Bodegas (nombre, ubicacion) VALUES ('Bodega Norte', 'Norteamerica');
+INSERT INTO Bodegas (nombre, ubicacion) VALUES ('Bodega Sur', 'Suramerica');
+INSERT INTO Bodegas (nombre, ubicacion) VALUES ('Bodega Caribe', 'Islas del Caribe');
+
+-- Empleados en Bodega Norte
+INSERT INTO Empleados (nombre, rol, salario, moneda, bodegaId) VALUES ('Juan Perez', 'bodeguero', 3000.00, 'USD', 1);
+INSERT INTO Empleados (nombre, rol, salario, moneda, bodegaId) VALUES ('Ana Gomez', 'jefe de logistica', 4500.00, 'USD', 1);
+-- Productos en Bodega Norte
+INSERT INTO Productos (nombre, descripcion, cantidad, afinidadId) VALUES ('laptop hp', 'laptop hp modelo x123', 40, 1);
+INSERT INTO Productos (nombre, descripcion, cantidad, afinidadId) VALUES ('camiseta algodon', 'camiseta de algodon talla m', 150, 2);
+
+-- Empleados en Bodega Sur
+INSERT INTO Empleados (nombre, rol, salario, moneda, bodegaId) VALUES ('Carlos Ruiz', 'bodeguero', 2500.00, 'COP', 2);
+INSERT INTO Empleados (nombre, rol, salario, moneda, bodegaId) VALUES ('Luisa Martinez', 'jefe de bodega', 4000.00, 'COP', 2);
+-- Productos en Bodega Sur
+INSERT INTO Productos (nombre, descripcion, cantidad, afinidadId) VALUES ('smartphone samsung', 'smartphone samsung galaxy s10', 30, 1);
+INSERT INTO Productos (nombre, descripcion, cantidad, afinidadId) VALUES ('jeans levi', 'jeans levi talla 32', 100, 2);
+
+-- Empleados en Bodega Caribe
+INSERT INTO Empleados (nombre, rol, salario, moneda, bodegaId) VALUES ('Marcos Diaz', 'bodeguero', 3500.00, 'EUR', 3);
+INSERT INTO Empleados (nombre, rol, salario, moneda, bodegaId) VALUES ('Sofia Castro', 'administrativo', 3000.00, 'EUR', 3);
+-- Productos en Bodega Caribe
+INSERT INTO Productos (nombre, descripcion, cantidad, afinidadId) VALUES ('tablet apple', 'tablet apple ipad 10 pulgadas', 20, 1);
+INSERT INTO Productos (nombre, descripcion, cantidad, afinidadId) VALUES ('gorra', 'gorra de beisbol', 200, 2);
+
+
+
